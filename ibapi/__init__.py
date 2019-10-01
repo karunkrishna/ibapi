23e9acd9c2448b078486f7915a1f9848e47842ac
Karunyan Krishnasamy <karun.krishna@gmail.com>
2019-10-01 13:44:59 -0400

    Added __str__ to order_state class

--- ibapi/__init__.py
+++ ibapi/__init__.py
@@ -8,7 +8,7 @@ Copyright (C) 2019 Interactive Brokers LLC. All rights reserved. This code is su
 VERSION = {
     'major': 9,
     'minor': 76,
-    'micro': 1}
+    'micro': 2}
 
 
 def get_version_string():
